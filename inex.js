7f5e7e989e395fa6f039fa7d9589cb6f6b170da0
Shivamintuu <145428777+Shivamintuu@users.noreply.github.com>
2024-07-20 20:03:25 +0530

    Add files via upload

---
+++ inex.js
@@ -0,0 +1,50 @@
+const apikey ="ce3af445b048a82000b3bf94c050900b";
+const apiurl ="https://api.openweathermap.org/data/2.5/weather?units=metrics&q=";
+
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector("button");
+const weathericon=document.querySelector(".weatherimg")
+async function chechweather(city){
+    const response= await fetch(apiurl + city +`&appid=${apikey}`);
+    var data =await response.json();
+    console.log(data);
+    
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)/10+"℃";
+    document.querySelector(".humidity1").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind1").innerHTML=data.wind.speed+"km/hr";
+    document.querySelector(".city").innerHTML=data.name;
+
+
+
+
+
+     if(data.weather[0].main="clouds"){
+       weathericon.src="/cloud.png";
+
+     }
+     else if(data.weather[0].main="clear"){
+       weathericon.src="/clear.png";
+
+     }
+     else if(data.weather[0].main="mist"){
+       weathericon.src="/mist.png";
+
+     }
+     else if(data.weather[0].main="rain"){
+       weathericon.src="/rain.png";
+
+     }
+     else if(data.weather[0].main="snow"){
+       weathericon.src="/snow.png";
+
+     }
+     
+    
+     
+    }
+    searchbtn.addEventListener("click", ()=>{
+        chechweather(searchbox.value);
+
+    })
+
